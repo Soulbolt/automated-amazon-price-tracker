f5b0bd0f124a0029bbaa0747a1942eaa4147d123
Soulbolt <soulbolt@gmail.com>
2024-01-04 11:16:48 -0700

    scraped for product price as float by adding header info. WIP if statement to check when price drops below target price.

--- main.py
+++ main.py
@@ -2,10 +2,17 @@ from bs4 import BeautifulSoup
 import requests
 
 AMAZON_PRODUCT = "https://www.amazon.com/Logitech-G502-Performance-Gaming-Mouse/dp/B07GBZ4Q68/"
-
-response = requests.get(url=AMAZON_PRODUCT)
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+    "Accept-Language": "en-US,en;q=0.9"
+}
+response = requests.get(url=AMAZON_PRODUCT, headers=headers)
 response.raise_for_status
 content = response.text
 
 soup = BeautifulSoup(content, "html.parser")
-print(soup)
+product_price = float(soup.find(name="span", class_="a-price-whole").getText().split(".")[0])
+print(product_price)
+
+if product_price > 50:
+    pass
